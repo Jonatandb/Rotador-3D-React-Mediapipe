1e4415abf0213c7d05531802da0353b018c45f60
Jonatandb <jonatandb@gmail.com>
2024-08-24 20:06:26 -0300

    Update repo name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.gltf'],
-  base: "/Rotador-3d-React-Mediapipe",
+  base: "/Rotador-3D-React-Mediapipe",
 })
