8709f8206a8c9fedbf5c66f0a6a3c920ca48f680
Jonatandb <jonatandb@gmail.com>
2024-08-24 21:00:29 -0300

    Github Pages config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.gltf'],
-  base: "https://jonatandb.dev.ar/Rotador-3D-React-Mediapipe",
+  base: "./",
 })
